cf5558a2e09b0220e08597cc01821db7be39b5ef
Rushil Shakya <rushil.shakya@gmail.com>
2020-04-23 19:08:27 +0545

    feat: get block layouts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+
+import "@wordpress/block-library/build-style/style.css"
 import "../styles/layout.css"
 
 const Layout = ({ children }) => {
